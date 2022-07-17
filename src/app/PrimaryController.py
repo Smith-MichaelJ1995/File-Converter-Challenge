860634f2eeb258d8374398444b411ecb6adb2b5d
Michael Smith <michaeljoshuasmith1@gmail.com>
2022-07-17 15:09:55 -0400

    Able to upload file to server through HTML form, save to server in Python.

--- src/app/PrimaryController.py
+++ src/app/PrimaryController.py
@@ -2,10 +2,10 @@
 # import requests
 import json
 import os
-from tkinter import *
+from werkzeug.utils import secure_filename
 
 # integrate Flask API Class Libraries
-from flask import Flask, jsonify, redirect, render_template, request
+from flask import Flask, jsonify, render_template, request
 from flask_classful import FlaskView, route
 
 # integrate Database & MarvelController class controller
@@ -20,12 +20,17 @@ app = Flask(__name__)
 # Controller Class To Integrate with UI, MarvelAPI Backend, Local Database.
 class PrimaryController(FlaskView):
 
+    def __init__(self) -> None:
+        # Create a directory in a known location to save files to.
+        self.uploads_dir = os.path.join(app.instance_path, 'uploads')
+        os.makedirs(self.uploads_dir, exist_ok=True)
+
     # ROUTE #0: DISPLAY ALL TABLES (CHARACTERS) IN DATABASE AS CLICKABLE LINKS
     @route('/', methods=["GET"])
     def index(self):
-        print("READING FROM INDEX FILE")
-        return render_template("index.html")
 
+        # read base-template
+        return render_template("index.html")
 
     # ROUTE #1: FETCH FILE FROM DATABASE BY ID. DISPLAY IN INDEX & RETURN METADATA IN HEADERS (UNIT TEST SUPPORT)
     @route('/file/<name>', methods=['GET'])
@@ -36,16 +41,17 @@ class PrimaryController(FlaskView):
 
 
     # RE-ROUTE TO HOME PAGE             
-    @route('/file', methods=['PUT'])
+    @route('/file', methods=['POST'])
     def createView(self):
 
-        print("HELLO FROM FILE PUT FUNCTION!!!!!")
-        print(request.data)
-        
-     
-        # RE-ROUTE TO VIEW PAGE 
-        # character already exists, return back existing page
-        return "{}".format(request.data)
+        f = request.files['file']
+        f.save(
+            os.path.join(
+                self.uploads_dir,
+                secure_filename(f.filename)
+            )
+        )
+        return 'file uploaded successfully'
 
 
 # instantiate PrimaryController FlaskAPI Instance
@@ -53,6 +59,7 @@ PrimaryController.register(app, route_base='/')
 
 # trigger invocation of API
 if __name__ == '__main__':
+
+    # instantiate flask application
     app.run(host='0.0.0.0', port=5050, debug=True)
-    #instantiate neighboring controller classes
     
